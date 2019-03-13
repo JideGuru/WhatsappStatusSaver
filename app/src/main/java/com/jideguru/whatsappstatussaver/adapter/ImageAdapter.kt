c16f45803526333453dca461c71dc7e38b5d7ea7
guruliciousjide@gmail.com <guruliciousjide@gmail.com>
2019-03-13 15:16:21 +0100

    Added Thumnails to video files

--- app/src/main/java/com/jideguru/whatsappstatussaver/adapter/ImageAdapter.kt
+++ app/src/main/java/com/jideguru/whatsappstatussaver/adapter/ImageAdapter.kt
@@ -8,7 +8,10 @@ import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.img_item.view.*
 import java.io.File
 import android.graphics.BitmapFactory
-
+import android.provider.MediaStore
+import android.R.attr.path
+import android.media.ThumbnailUtils
+import android.widget.Toast
 
 
 class ImageAdapter(val items : Array<File>, val context: Context) : RecyclerView.Adapter<ImageAdapter.ViewHolder>() {
@@ -18,11 +21,23 @@ class ImageAdapter(val items : Array<File>, val context: Context) : RecyclerView
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder?.filePath?.text = items[position].name
+        holder.filePath?.text = items[position].name
+        Toast.makeText(context, items[position].extension, Toast.LENGTH_LONG).show()
 
-        val myBitmap = BitmapFactory.decodeFile(items[position].absolutePath) //this is the bitmap for the image
+        if(items[position].extension == "mp4"){
+            val thumb = ThumbnailUtils.createVideoThumbnail(
+                items[position].absolutePath,
+                MediaStore.Images.Thumbnails.MINI_KIND
+            )
+            holder.fileThumb?.setImageBitmap(thumb)//image set to the image view
+        }else{
+            val myBitmap = BitmapFactory.decodeFile(items[position].absolutePath) //this is the bitmap for the image
 
-        holder?.fileThumb?.setImageBitmap(myBitmap)//image set to the image view
+            holder.fileThumb?.setImageBitmap(myBitmap)//image set to the image view
+        }
+
+
+
     }
 
     override fun getItemCount(): Int {
