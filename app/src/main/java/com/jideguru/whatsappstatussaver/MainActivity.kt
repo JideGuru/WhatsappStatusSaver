b2fd39782d8c341ce42b0981c5bd6163f1fad811
guruliciousjide@gmail.com <guruliciousjide@gmail.com>
2019-03-13 01:46:46 +0100

    Added File ArrayList

--- app/src/main/java/com/jideguru/whatsappstatussaver/MainActivity.kt
+++ app/src/main/java/com/jideguru/whatsappstatussaver/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.jideguru.whatsappstatussaver
 
+import android.app.ProgressDialog.show
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
@@ -21,6 +22,9 @@ class MainActivity : AppCompatActivity() {
 
         val WAdirectory = "${Environment.getExternalStorageDirectory()}/WhatsApp"
         val WABdirectory = "${Environment.getExternalStorageDirectory()}/WhatsApp Business"
+
+        val WAStatus = "${Environment.getExternalStorageDirectory()}/WhatsApp/Media/.Statuses"
+        val WABStatus = "${Environment.getExternalStorageDirectory()}/WhatsApp Business/Media/.Statuses"
 //        Log.i("DIRECTORY",WAdirectory)
 
 
@@ -29,11 +33,35 @@ class MainActivity : AppCompatActivity() {
 
 //        var fileExists = file.exists()
 
-        when {
-            WA.isDirectory -> Toast.makeText(this, "WhatsApp is detected", Toast.LENGTH_SHORT).show()
-            WAB.isDirectory -> Toast.makeText(this, "WhatsApp is detected Business", Toast.LENGTH_SHORT).show()
-            else -> Toast.makeText(this, "Please install Whatsapp to use this app", Toast.LENGTH_SHORT).show()
+        if(WA.isDirectory){
+            Toast.makeText(this, "WhatsApp is detected", Toast.LENGTH_SHORT).show()
+            fileReader(File(WAStatus))
+        }else if(WAB.isDirectory ){
+            Toast.makeText(this, "WhatsApp is detected Business", Toast.LENGTH_SHORT).show()
+        } else{
+            Toast.makeText(this, "Please install WhatsApp to use this app", Toast.LENGTH_SHORT).show()
         }
 
+        File(WAStatus).walkTopDown().forEach { Log.i("DIRECTORY", it.toString()) }
+
+
+    }
+
+    fun fileReader(root: File) {
+        val fileList: ArrayList<File> = ArrayList()
+        val listAllFiles = root.listFiles()
+
+        if (listAllFiles != null && listAllFiles.size > 0) {
+            for (currentFile in listAllFiles) {
+                if (currentFile.name.endsWith(".jpeg")) {
+                    // File absolute path
+                    Log.e("downloadFilePath", currentFile.getAbsolutePath())
+                    // File Name
+                    Log.e("downloadFileName", currentFile.getName())
+                    fileList.add(currentFile.absoluteFile)
+                }
+            }
+            Log.w("fileList", "" + fileList.size)
+        }
     }
 }
